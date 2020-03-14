f0a21676e28346e22cbf0b4f392799f00cde4f46
seba_3567 <sebastian26481@gmail.com>
2020-03-14 09:17:40 -0300

    rm moto actions and prepare build derplab

--- derp_doha.mk
+++ derp_doha.mk
@@ -21,20 +21,25 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-VENDOR_EXCEPTION_PATHS := omni \
+VENDOR_EXCEPTION_PATHS := aosip \
     motorola \
     gapps \
     microg
 
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
+#$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
@@ -43,10 +48,10 @@ TARGET_BOOTANIMATION_SIZE := 1080p
 AB_OTA_UPDATER := true
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/doha/overlay/device
-DEVICE_PACKAGE_OVERLAYS += vendor/omni/overlay/CarrierConfig
+#DEVICE_PACKAGE_OVERLAYS += vendor/omni/overlay/CarrierConfig
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+#$(call inherit-product, vendor/omni/config/common.mk)
 
 # get the rest of aosp stuff after ours
 # $(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system_arm64.mk)
@@ -55,7 +60,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/motorola/doha/device.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := omni_doha
+PRODUCT_NAME := derp_doha
 PRODUCT_DEVICE := doha
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
@@ -66,8 +71,8 @@ PRODUCT_SYSTEM_NAME := Moto G8 Plus
 
 VENDOR_RELEASE := 9/PPIS29.65-24-2/dc7f7b:user/release-keys
 BUILD_FINGERPRINT := motorola/doha_retail/doha:$(VENDOR_RELEASE)
-OMNI_BUILD_FINGERPRINT := motorola/doha_retail/doha:$(VENDOR_RELEASE)
-OMNI_PRIVATE_BUILD_DESC := "'doha_retail-user 10 PPIS29.65-24-2 dc7f7b release-keys'"
+#OMNI_BUILD_FINGERPRINT := motorola/doha_retail/doha:$(VENDOR_RELEASE)
+#OMNI_PRIVATE_BUILD_DESC := "'doha_retail-user 10 PPIS29.65-24-2 dc7f7b release-keys'"
 
 PLATFORM_SECURITY_PATCH_OVERRIDE := 2019-11-01
 
@@ -75,13 +80,6 @@ TARGET_VENDOR := motorola
 
 $(call inherit-product, vendor/motorola/doha/doha-vendor.mk)
 
-ifeq ($(WITH_GAPPS),true)
-# https://gitlab.com/darkobas/android_vendor_gapps
-$(call inherit-product, vendor/gapps/config.mk)
-endif
-
-ifeq ($(WITH_MICROG),true)
-# https://github.com/boulzordev/android_prebuilts_prebuiltapks
-$(call inherit-product, vendor/microg/microg.mk)
-endif
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
 
